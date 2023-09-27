f2d3291565b40c5f92380bc86b00791d29798a69
Kurt Ivan A. Lopez <145944343+kialopez@users.noreply.github.com>
2023-09-27 08:46:33 +0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+#name here
+name = input("What is your name?")
+
+#age here
+age_input = input("What is your age")
+age = int(age_input)
+age_next_year = age+1
+
+modulo = 5%3
+
+pi = 3.1416
+
+print(f"Hello,"+name)
+print(f"You are ",age_next_year,"years old next year")
+print(f"Pi value is {pi}")
+print(f"5 modulo 3 is {modulo}")
